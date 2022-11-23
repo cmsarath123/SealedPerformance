1fc4d470f31ed3e6fbd8e9b2cdbe8f4722d258ab
cmsar <cmsar@SARATH-PC>
2022-11-23 15:13:00 +0530

    Add project files.

---
+++ SealedPerformance/Benchmarks.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SealedPerformance
+{
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        private readonly BaseClass _baseClass = new();
+        private readonly NormalClass _normalClass = new();
+        private readonly SealedClass _sealedClass = new();
+
+        private readonly NormalClass[] _normalClasses = new NormalClass[1];
+        private readonly SealedClass[] _sealedClasses = new SealedClass[1];
+
+        [Benchmark]
+        public void NormalVoid() => _normalClass.VoidMethod();
+
+        [Benchmark]
+        public void SealedVoid() => _sealedClass.VoidMethod();
+
+        [Benchmark]
+        public int NormalInt() => _normalClass.IntMethod2()+100;
+
+        [Benchmark]
+        public int SealedInt() => _sealedClass.IntMethod2()+100;
+
+        [Benchmark]
+        public bool IsNormal() => _baseClass is NormalClass;
+
+        [Benchmark]
+        public bool IsSealed() => _baseClass is SealedClass;
+
+        [Benchmark]
+        public void NormalArray() => _normalClasses[0] = _normalClass;
+
+        [Benchmark]
+        public void SealedArray() => _sealedClasses[0] = _sealedClass;
+    }
+}
---
+++ SealedPerformance/TestClasses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedPerformance
+{
+    public class BaseClass
+    {
+        public virtual void VoidMethod() { }
+        public virtual int IntMethod2() => 0;
+    }
+
+    public class NormalClass: BaseClass
+    {
+        public override void VoidMethod() { }
+        public override int IntMethod2() => 69;
+    }
+
+    public sealed class SealedClass: BaseClass
+    {
+        public override void VoidMethod() { }
+        public override int IntMethod2() => 69;
+    }
+}
